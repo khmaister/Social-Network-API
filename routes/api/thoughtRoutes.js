abc9552115e42054f9a4670d5e22d881fcc95b4a
van khin <khmaister@yahoo.com>
2024-02-17 11:46:06 -0500

    thought routes defined

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thoughtController");
+
+// Get all thoughts
+router.get(getAllThoughts);
+
+// Get thought by id
+router.get("/:thoughtId", getThoughtById);
+
+// Create thought
+router.post("/", createThought);
+
+//Update thought
+router.put("/:thoughtId", updateThought);
+
+// Delete thought by Id
+router.delete("/:thoughtId", deleteThought);
+
