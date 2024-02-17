fcafaa7225cc66ed0cb8db48ae2eb86ead59dd4c
van khin <khmaister@yahoo.com>
2024-02-17 09:57:48 -0500

    user and index models created

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+      username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        ],
+      },
+      thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "Thought",
+        },
+      ],
+      friends: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+    },
+    {
+      toJSON: {
+        virtuals: true,
+        getters: true,
+      },
+      id: false,
+    }
+  );
+const User = model("User", userSchema);
+module.exports = User;
