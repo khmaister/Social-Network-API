ac657af02175b6a65f68fb76325ad7e8c30263b2
van khin <khmaister@yahoo.com>
2024-02-17 10:13:37 -0500

    thought model created

---
+++ models/thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+function dateFormat(date) {
+    return date.toLocaleString();
+};
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [reactionSchema],
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
